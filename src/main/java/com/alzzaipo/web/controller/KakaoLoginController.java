e0679ac2cb69cf11b386657a19db994319e095d9
csct3434 <csct3434@gmail.com>
2023-02-28 19:37:36 +0900

    fixed

--- src/main/java/com/alzzaipo/web/controller/KakaoLoginController.java
+++ src/main/java/com/alzzaipo/web/controller/KakaoLoginController.java
@@ -3,9 +3,13 @@ package com.alzzaipo.web.controller;
 import com.alzzaipo.config.SessionConfig;
 import com.alzzaipo.service.KakaoLoginService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,14 +28,22 @@ public class KakaoLoginController {
     }
 
     @GetMapping("/kakao_callback")
-    public String kakaoLogin(@RequestParam("code") String code, HttpSession session) throws JsonProcessingException {
+    public ResponseEntity kakaoLogin(@RequestParam("code") String code, HttpSession session, HttpServletResponse response) throws JsonProcessingException {
         kakaoLoginService.kakaoLogin(code, session);
 
         String sessionAccessToken = (String)session.getAttribute(SessionConfig.accessToken);
         Long sessionMemberId = (Long) session.getAttribute(SessionConfig.memberId);
+
+        Cookie cookie = new Cookie("refreshToken", "This_is_test_refreshToken!!");
+        cookie.setMaxAge(7 * 24 * 60 * 60);
+        cookie.setSecure(true);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setAttribute("JSESSIONID", session.getId());
+        response.addCookie(cookie);
         log.info("session established - memberId:" + sessionMemberId + " / accessToken:" + sessionAccessToken);
 
-        return "index";
+        return new ResponseEntity<>("access Token", HttpStatus.OK);
     }
 
     @GetMapping("/logout")
