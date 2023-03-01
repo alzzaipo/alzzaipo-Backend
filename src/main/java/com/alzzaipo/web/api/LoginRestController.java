00424213a43960213fe9db10b5f447cd68e1ed38
csct3434 <csct3434@gmail.com>
2023-03-01 19:41:54 +0900

    update getLoginStatus API to include nickname

--- src/main/java/com/alzzaipo/web/api/LoginRestController.java
+++ src/main/java/com/alzzaipo/web/api/LoginRestController.java
@@ -1,6 +1,8 @@
 package com.alzzaipo.web.api;
 
+import com.alzzaipo.config.SessionConfig;
 import com.alzzaipo.config.SessionManager;
+import com.alzzaipo.web.dto.LoginStatusDto;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +16,11 @@ public class LoginRestController {
     private final SessionManager sessionManager;
 
     @GetMapping("/api/getLoginStatus")
-    public ResponseEntity<Boolean> getLoginStatus(HttpSession session) {
-
-        Boolean login = sessionManager.verifySession(session);
-
-        return ResponseEntity.ok(login);
+    public ResponseEntity<LoginStatusDto> getLoginStatus(HttpSession session) {
+        if(sessionManager.verifySession(session)) {
+            return ResponseEntity.ok(new LoginStatusDto(true, (String)session.getAttribute(SessionConfig.nickname)));
+        } else {
+            return ResponseEntity.ok(new LoginStatusDto(false, "none"));
+        }
     }
 }
