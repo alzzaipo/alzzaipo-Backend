77c0130cd80592d00f2465cf6c1560f2c134b61d
csct3434 <csct3434@gmail.com>
2023-03-17 04:14:31 +0900

    Remove: 포트폴리오 API와 무관한 코드 삭제

--- src/main/java/com/alzzaipo/domain/portfolio/PortfolioRepository.java
+++ src/main/java/com/alzzaipo/domain/portfolio/PortfolioRepository.java
@@ -1,14 +1,6 @@
 package com.alzzaipo.domain.portfolio;
 
-import com.alzzaipo.domain.dto.PortfolioUpdateResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.Optional;
 
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
-
-    @Query(value = "SELECT new com.alzzaipo.domain.dto.PortfolioUpdateResponseDto(p.id, p.ipo.stockCode, p.sharesCnt, p.profit, p.agents, p.memo)" +
-            "FROM Portfolio p WHERE p.id = :portfolioId")
-    public Optional<PortfolioUpdateResponseDto> getPortfolioUpdateResponseDto(Long portfolioId);
 }
