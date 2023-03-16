95705bc6dab3fbeaddeade268d18f3f1841edc45
csct3434 <csct3434@gmail.com>
2023-03-17 03:53:41 +0900

    Remove: Session 관련 설정 파일 삭제

--- src/main/java/com/alzzaipo/config/SessionConfig.java
+++
@@ -1,8 +0,0 @@
-package com.alzzaipo.config;
-
-public class SessionConfig {
-
-    public static String memberId = "memberId";
-    public static String nickname = "nickname";
-    public static String kakaoAccountId = "kakaoAccountId";
-}
