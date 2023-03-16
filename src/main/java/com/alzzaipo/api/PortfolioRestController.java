5bbb34e5fdd8ef7280b8c9202959b279f30915d4
csct3434 <csct3434@gmail.com>
2023-03-17 04:10:32 +0900

    Add: 포트폴리오 CRUD 검증 코드 변경 및 추가

--- src/main/java/com/alzzaipo/api/PortfolioRestController.java
+++ src/main/java/com/alzzaipo/api/PortfolioRestController.java
@@ -1,70 +1,50 @@
 package com.alzzaipo.api;
 
-import com.alzzaipo.config.SessionManager;
-import com.alzzaipo.domain.dto.PortfolioUpdateResponseDto;
-import com.alzzaipo.service.PortfolioService;
 import com.alzzaipo.domain.dto.PortfolioCreateRequestDto;
 import com.alzzaipo.domain.dto.PortfolioListResponseDto;
+import com.alzzaipo.domain.dto.PortfolioUpdateRequestDto;
+import com.alzzaipo.service.PortfolioService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/portfolio")
 @RestController
 public class PortfolioRestController {
 
     private final PortfolioService portfolioService;
-    private final SessionManager sessionManager;
 
     @GetMapping("/list")
     public ResponseEntity<List<PortfolioListResponseDto>> getMemberPortfolios(@AuthenticationPrincipal String accountId) {
 
         List<PortfolioListResponseDto> portfolioListResponseDtos =
-                portfolioService.getMemberPortfolioListResponseDtos(accountId);
+                portfolioService.getMemberPortfolioList(accountId);
 
         return ResponseEntity.ok(portfolioListResponseDtos);
     }
 
     @PostMapping("/create")
-    public ResponseEntity createMemberPortfolio(@AuthenticationPrincipal String accountId, PortfolioCreateRequestDto portfolioCreateRequestDto) {
-
-        Boolean success = portfolioService.createMemberPortfolio(accountId, portfolioCreateRequestDto);
-
-        if(success) {
-            return ResponseEntity.ok().build();
-        } else {
-            return ResponseEntity.badRequest().build();
-        }
+    public ResponseEntity<String> createMemberPortfolio(@AuthenticationPrincipal String accountId,
+                                                        @RequestBody PortfolioCreateRequestDto portfolioCreateRequestDto) {
+        portfolioService.createMemberPortfolio(accountId, portfolioCreateRequestDto);
+        return ResponseEntity.ok().body("포트폴리오 생성 완료");
     }
 
     @PutMapping("/update")
-    public ResponseEntity updateMemberPortfolio(@AuthenticationPrincipal String accountId, PortfolioUpdateResponseDto portfolioUpdateResponseDto) {
-        Boolean success = portfolioService.updateMemberPortfolio(accountId, portfolioUpdateResponseDto);
-
-        if(success) {
-            return ResponseEntity.ok().build();
-        } else {
-            return ResponseEntity.badRequest().build();
-        }
+    public ResponseEntity<String> updateMemberPortfolio(@AuthenticationPrincipal String accountId,
+                                                        @RequestBody PortfolioUpdateRequestDto portfolioUpdateRequestDto) {
+        portfolioService.updateMemberPortfolio(accountId, portfolioUpdateRequestDto);
+        return ResponseEntity.ok().body("포트폴리오 수정 완료");
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity delete(@AuthenticationPrincipal String accountId, @RequestParam("portfolioId") Long portfolioId) {
-
-        boolean isPortfolioDeleted  = portfolioService.deleteMemberPortfolio(accountId, portfolioId);
-
-        if(isPortfolioDeleted) {
-            return ResponseEntity.ok().build();
-        } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
+    public ResponseEntity<String> delete(@AuthenticationPrincipal String accountId, @RequestParam("portfolioId") Long portfolioId) {
+        portfolioService.deleteMemberPortfolio(accountId, portfolioId);
+        return ResponseEntity.ok().body("포트폴리오 삭제 완료");
     }
 
 }
--- src/main/java/com/alzzaipo/service/PortfolioService.java
+++ src/main/java/com/alzzaipo/service/PortfolioService.java
@@ -1,7 +1,7 @@
 package com.alzzaipo.service;
 
 import com.alzzaipo.domain.dto.PortfolioListResponseDto;
-import com.alzzaipo.domain.dto.PortfolioUpdateResponseDto;
+import com.alzzaipo.domain.dto.PortfolioUpdateRequestDto;
 import com.alzzaipo.domain.ipo.IPO;
 import com.alzzaipo.domain.ipo.IPORepository;
 import com.alzzaipo.domain.member.Member;
@@ -9,15 +9,15 @@ import com.alzzaipo.domain.member.MemberRepository;
 import com.alzzaipo.domain.portfolio.Portfolio;
 import com.alzzaipo.domain.portfolio.PortfolioRepository;
 import com.alzzaipo.domain.dto.PortfolioCreateRequestDto;
+import com.alzzaipo.exception.AppException;
+import com.alzzaipo.exception.ErrorCode;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -32,123 +32,128 @@ public class PortfolioService {
     private final MemberRepository memberRepository;
 
     public Portfolio save(Portfolio portfolio) {
+        // 생성 요청이면 save로 신규 저장
         if(portfolio.getId() == null) {
             em.persist(portfolio);
             return portfolio;
         }
+        // 수정 요청이면 merge로 수정 저장
         else {
             return em.merge(portfolio);
         }
     }
 
-    public Boolean createMemberPortfolio(String accountId, PortfolioCreateRequestDto createRequestDto) {
-        IPO ipo = ipoRepository.findByStockCode(createRequestDto.getStockCode())
-                .orElseGet(() -> new IPO());
+    public Portfolio createMemberPortfolio(String accountId, PortfolioCreateRequestDto createRequestDto) {
 
-        Member member = memberRepository.findByAccountId(accountId)
-                .orElseGet(() -> new Member());
+        // 계정 ID 검증
+        Member member = getMemberAfterValidation(accountId);
 
-        if(ipo.getId() == null) {
-            log.warn("Invalid IPO");
-            return false;
-        }
-        else if(member.getId() == null) {
-            log.warn("Invalid memberId");
-            return false;
-        }
-        else {
-            Portfolio portfolio = Portfolio.builder()
-                    .member(member)
-                    .ipo(ipo)
-                    .sharesCnt(createRequestDto.getSharesCnt())
-                    .profit(createRequestDto.getProfit())
-                    .agents(createRequestDto.getAgents())
-                    .memo(createRequestDto.getMemo())
-                    .build();
+        // 종목코드 검증
+        IPO ipo = getIpoAfterValidation(createRequestDto.getStockCode());
 
-            member.addPortfolio(portfolio);
-            portfolioRepository.save(portfolio);
-            return true;
-        }
+        // DTO로부터 새로운 포트폴리오 생성
+        Portfolio portfolio = Portfolio.builder()
+                .member(member)
+                .ipo(ipo)
+                .sharesCnt(createRequestDto.getSharesCnt())
+                .profit(createRequestDto.getProfit())
+                .agents(createRequestDto.getAgents())
+                .memo(createRequestDto.getMemo())
+                .build();
+
+        // 연관정보 추가
+        member.addPortfolio(portfolio);
+
+        // 포트폴리오 저장
+        return portfolioRepository.save(portfolio);
     }
 
-    public Boolean updateMemberPortfolio(String accountId, PortfolioUpdateResponseDto portfolioUpdateResponseDto) {
-        if(portfolioUpdateResponseDto == null) {
-            log.error("PortfolioSaveRequestDto is null");
-            return false;
-        }
+    public Portfolio updateMemberPortfolio(String accountId, PortfolioUpdateRequestDto portfolioUpdateRequestDto) {
+        // 계정 ID 검증
+        Member member = getMemberAfterValidation(accountId);
 
-        if(portfolioRepository.findById(portfolioUpdateResponseDto.getPortfolioId()).isEmpty()) {
-            log.error("Invalid PortfolioId - portfolioUpdateDto.getPortfolioId()" + portfolioUpdateResponseDto.getPortfolioId());
-            return false;
-        }
+        // 포트폴리오 ID 검증
+        Portfolio oldPortfolio = getPortfolioAfterValidation(portfolioUpdateRequestDto.getPortfolioId());
 
-        IPO ipo = ipoRepository.findByStockCode(portfolioUpdateResponseDto.getStockCode())
-                .orElseGet(() -> new IPO());
+        // 종목코드 검증
+        IPO ipo = getIpoAfterValidation(portfolioUpdateRequestDto.getStockCode());
 
-        Member member = memberRepository.findByAccountId(accountId)
-                .orElseGet(() -> new Member());
+        // 포트폴리오 소유권 검증
+        validatePortfolioOwnership(accountId, oldPortfolio.getMember().getAccountId());
 
-        if(ipo.getId() == null) {
-            log.warn("Invalid IPO");
-            return false;
-        }
-        else if(member.getId() == null) {
-            log.warn("Invalid memberId");
-            return false;
-        }
-        else {
-            Portfolio portfolio = Portfolio.builder()
-                    .member(member)
-                    .ipo(ipo)
-                    .sharesCnt(portfolioUpdateResponseDto.getSharesCnt())
-                    .profit(portfolioUpdateResponseDto.getProfit())
-                    .agents(portfolioUpdateResponseDto.getAgents())
-                    .memo(portfolioUpdateResponseDto.getMemo())
-                    .build();
+        // 수정된 내용의 포트폴리오 생성
+        Portfolio newPortfolio = Portfolio.builder()
+                .member(member)
+                .ipo(ipo)
+                .sharesCnt(portfolioUpdateRequestDto.getSharesCnt())
+                .profit(portfolioUpdateRequestDto.getProfit())
+                .agents(portfolioUpdateRequestDto.getAgents())
+                .memo(portfolioUpdateRequestDto.getMemo())
+                .build();
 
-            portfolio.changeId(portfolioUpdateResponseDto.getPortfolioId());
-            save(portfolio);
-            return true;
-        }
+        // 포트폴리오 수정 저장
+        newPortfolio.updateId(portfolioUpdateRequestDto.getPortfolioId());
+        save(newPortfolio);
+
+        return newPortfolio;
     }
 
     public boolean deleteMemberPortfolio(String accountId, Long portfolioId) {
-        Portfolio portfolio = portfolioRepository.findById(portfolioId)
-                .orElseGet(() -> new Portfolio());
+        // 계정 ID 검증
+        Member member = getMemberAfterValidation(accountId);
 
-        if(portfolio.getId() == null) {
-            log.warn("Invalid portfolioId");
-            return false;
-        }
-        else if(accountId.equals(portfolio.getMember().getAccountId())) {
-            portfolioRepository.delete(portfolio);
-            return true;
-        }
-        else {
-            log.warn("AccountId not match with portfolio owner - accountId:" + accountId + " ownerId:" + portfolio.getMember().getAccountId());
-            return false;
-        }
+        // 포트폴리오 ID 검증
+        Portfolio portfolio = getPortfolioAfterValidation(portfolioId);
+
+        // 포트폴리오 소유권 검증
+        validatePortfolioOwnership(accountId, portfolio.getMember().getAccountId());
+
+        // 포트폴리오 삭제
+        portfolioRepository.delete(portfolio);
+
+        return true;
     }
 
+    /* 회원의 포트폴리오 목록 반환 */
     @Transactional(readOnly = true)
-    public List<PortfolioListResponseDto> getMemberPortfolioListResponseDtos(String accountId) {
-        Member member = memberRepository.findByAccountId(accountId)
-                .orElseGet(() -> new Member());
+    public List<PortfolioListResponseDto> getMemberPortfolioList(String accountId) {
+        // 계정 ID 검증
+        Member member = getMemberAfterValidation(accountId);
 
-        if(member.getId() == null) {
-            log.warn("Invalid memberId");
-            return new ArrayList<>();
-        }
-        else {
-            return member.getPortfolios().stream()
+        // 포트폴리오를 DTO로 변환하여 반환
+        return member.getPortfolios().stream()
                     .map(Portfolio::toDto)
                     .collect(Collectors.toList());
+    }
+
+    // 계정 ID 검증 후, 해당하는 Member Entity 반환
+    private Member getMemberAfterValidation(String accountId) {
+        Member member = memberRepository.findByAccountId(accountId)
+                .orElseThrow(() -> new AppException(ErrorCode.ACCOUNT_ID_NOT_FOUND,
+                        accountId + ": 존재하지 않는 회원입니다."));
+        return member;
+    }
+
+    // 종목코드 검증 후, 해당하는 IPO Entity 반환
+    private IPO getIpoAfterValidation(int stockCode) {
+        IPO ipo = ipoRepository.findByStockCode(stockCode)
+                .orElseThrow(() -> new AppException(ErrorCode.IPO_NOT_FOUND,
+                        stockCode + ": 존재하지 않는 종목코드 입니다"));
+        return ipo;
+    }
+
+    // 포트폴리오 ID 검증 후, 해당하는 Portfolio Entity 반환
+    private Portfolio getPortfolioAfterValidation(Long portfolioId) {
+        Portfolio portfolio = portfolioRepository.findById(portfolioId)
+                .orElseThrow(() -> new AppException(ErrorCode.PORTFOLIO_NOT_FOUND, "존재하지 않는 포트폴리오 입니다."));
+        return portfolio;
+    }
+
+    // 회원이 포트폴리오의 생성자인지 검증
+    private void validatePortfolioOwnership(String accountId, String portfolioOwnerAccountId) {
+        if(!accountId.equals(portfolioOwnerAccountId)) {
+            throw new AppException(ErrorCode.INVALID_ACCOUNT_ID, "해당 회원의 포트폴리오가 아닙니다.");
         }
     }
 
-    @Transactional(readOnly = true)
-    public Optional<PortfolioUpdateResponseDto> getPortfolioUpdateResponseDto(Long portfolioId) {
-        return portfolioRepository.getPortfolioUpdateResponseDto(portfolioId);
-    }
 }
