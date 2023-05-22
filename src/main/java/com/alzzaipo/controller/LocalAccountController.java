7c0b38b8f46f38604ba3270e873b325c88264dcb
csct3434 <csct3434@gmail.com>
2023-05-23 01:45:42 +0900

    Update : 로컬 계정 API 주소 접두사 변경(/api/member -> /api/local-member)

--- src/main/java/com/alzzaipo/controller/LocalAccountController.java
+++ src/main/java/com/alzzaipo/controller/LocalAccountController.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/member")
+@RequestMapping("/api/local-member")
 @Controller
 public class LocalAccountController {
 
