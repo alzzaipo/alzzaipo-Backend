591c5ff0f528eec532327c64649bfecb46e2c70e
csct3434 <csct3434@gmail.com>
2023-05-23 01:41:04 +0900

    Fix : 로컬 계정 비밀번호 변경 API 주소 오탈자 정정

--- src/main/java/com/alzzaipo/controller/LocalAccountController.java
+++ src/main/java/com/alzzaipo/controller/LocalAccountController.java
@@ -57,7 +57,7 @@ public class LocalAccountController {
         return ResponseEntity.ok().body("회원정보 수정 완료");
     }
 
-    @PostMapping("change-password")
+    @PostMapping("/change-password")
     public ResponseEntity<String> changePassword(@AuthenticationPrincipal Long memberId, @RequestBody LocalAccountPasswordChangeRequestDto dto) {
         localAccountService.changePassword(memberId, dto);
         return ResponseEntity.ok().body("비밀번호 변경 완료");
