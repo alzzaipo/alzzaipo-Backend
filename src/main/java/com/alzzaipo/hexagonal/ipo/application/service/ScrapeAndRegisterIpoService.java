d734b8f3a709e497cbb41796801584b9fcb51b7d
csct3434 <csct3434@gmail.com>
2023-11-02 03:30:50 +0900

    feat: 공모주 웹 스크래핑 어댑터 구현

---
+++ src/main/java/com/alzzaipo/hexagonal/ipo/application/service/ScrapeAndRegisterIpoService.java
@@ -0,0 +1,41 @@
+package com.alzzaipo.hexagonal.ipo.application.service;
+
+import com.alzzaipo.hexagonal.ipo.IpoMapper;
+import com.alzzaipo.hexagonal.ipo.application.port.in.ScrapeAndRegisterIposUseCase;
+import com.alzzaipo.hexagonal.ipo.application.port.out.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ScrapeAndRegisterIpoService implements ScrapeAndRegisterIposUseCase {
+
+    private final ScrapeIposPort scrapeIposPort;
+    private final FindIpoByStockCodePort findIpoByStockCodePort;
+    private final RegisterIpoPort registerIpoPort;
+    private final IpoMapper ipoMapper;
+
+    @Override
+    public int scrapeAndRegisterIposUseCase(ScrapeIposCommand scrapeIposCommand) {
+        List<ScrapeIpoResult> scrapeIpoResults
+                = scrapeIposPort.scrapeIpos(scrapeIposCommand);
+
+        int registeredCnt = (int) scrapeIpoResults.stream()
+                .filter(this::isNotRegistered)
+                .map(this.ipoMapper::toDomainEntity)
+                .peek(registerIpoPort::registerIpo)
+                .count();
+
+        return registeredCnt;
+    }
+
+    private boolean isNotRegistered(ScrapeIpoResult scrapeIpoResult) {
+        int stockCode = scrapeIpoResult.getStockCode();
+
+        return findIpoByStockCodePort
+                .findIpoByStockCodePort(stockCode)
+                .isEmpty();
+    }
+}
