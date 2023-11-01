7abb8dd3e7758fb0369da80c47b68879e9a474da
csct3434 <csct3434@gmail.com>
2023-11-02 03:37:42 +0900

    feat: 공모주 웹 스크래핑 컨트롤러 구현

---
+++ src/main/java/com/alzzaipo/hexagonal/ipo/adapter/in/web/ScrapeAndRegisterIposWebRequest.java
@@ -0,0 +1,15 @@
+package com.alzzaipo.hexagonal.ipo.adapter.in.web;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ScrapeAndRegisterIposWebRequest {
+
+    private String authorizationCode;
+    private int pageFrom;
+    private int pageTo;
+}
