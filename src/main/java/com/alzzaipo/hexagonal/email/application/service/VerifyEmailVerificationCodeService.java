dc6a97b69e5197b61336405b7216ea8b1edf94b8
csct3434 <csct3434@gmail.com>
2023-11-03 00:52:24 +0900

    feat: 이메일 인증코드 검증 서비스 구현

---
+++ src/main/java/com/alzzaipo/hexagonal/email/application/service/VerifyEmailVerificationCodeService.java
@@ -0,0 +1,36 @@
+package com.alzzaipo.hexagonal.email.application.service;
+
+import com.alzzaipo.hexagonal.email.application.port.in.VerifyEmailVerificationCodeUseCase;
+import com.alzzaipo.hexagonal.email.domain.EmailVerificationCode;
+import com.alzzaipo.hexagonal.email.application.port.out.FindEmailVerificationCodePort;
+import com.alzzaipo.hexagonal.email.application.port.out.SetEmailVerificationHistoryVerifiedPort;
+import com.alzzaipo.hexagonal.email.domain.Email;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class VerifyEmailVerificationCodeService implements VerifyEmailVerificationCodeUseCase {
+
+    private final FindEmailVerificationCodePort findEmailVerificationCodePort;
+    private final SetEmailVerificationHistoryVerifiedPort setEmailVerificationHistoryVerifiedPort;
+
+    @Override
+    public boolean verifyEmailVerificationCode(Email email, EmailVerificationCode verificationCode) {
+        String userProvidedVerificationCode = verificationCode.get();
+
+        Optional<String> validVerificationCode =
+                findEmailVerificationCodePort.findEmailVerificationCode(email);
+
+        if(validVerificationCode.isPresent() &&
+                userProvidedVerificationCode.equals(validVerificationCode.get()))
+        {
+            setEmailVerificationHistoryVerifiedPort.setEmailVerificationHistoryVerified(email);
+            return true;
+        }
+
+        return false;
+    }
+}
