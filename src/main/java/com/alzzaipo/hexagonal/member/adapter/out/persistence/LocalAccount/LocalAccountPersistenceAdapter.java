fcfe997fae31dd294c46b2a3a26a3c973aa000df
csct3434 <csct3434@gmail.com>
2023-11-03 04:57:14 +0900

    feat: 로컬 계정을 아이디로 조회하는 어댑터 구현

--- src/main/java/com/alzzaipo/hexagonal/member/adapter/out/persistence/LocalAccount/LocalAccountPersistenceAdapter.java
+++ src/main/java/com/alzzaipo/hexagonal/member/adapter/out/persistence/LocalAccount/LocalAccountPersistenceAdapter.java
@@ -1,11 +1,21 @@
 package com.alzzaipo.hexagonal.member.adapter.out.persistence.LocalAccount;
 
+import com.alzzaipo.domain.account.local.LocalAccount;
+import com.alzzaipo.hexagonal.member.application.port.out.FindLocalAccountByAccountIdPort;
+import com.alzzaipo.hexagonal.member.domain.LocalAccount.LocalAccountId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
-public class LocalAccountPersistenceAdapter {
+public class LocalAccountPersistenceAdapter implements FindLocalAccountByAccountIdPort {
 
     private final NewLocalAccountRepository localAccountRepository;
+
+    @Override
+    public Optional<LocalAccount> findLocalAccountByAccountId(LocalAccountId localAccountId) {
+        return localAccountRepository.findByAccountId(localAccountId.get());
+    }
 }
--- src/main/java/com/alzzaipo/hexagonal/member/adapter/out/persistence/LocalAccount/NewLocalAccountRepository.java
+++ src/main/java/com/alzzaipo/hexagonal/member/adapter/out/persistence/LocalAccount/NewLocalAccountRepository.java
@@ -1,6 +1,14 @@
 package com.alzzaipo.hexagonal.member.adapter.out.persistence.LocalAccount;
 
+import com.alzzaipo.domain.account.local.LocalAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface NewLocalAccountRepository extends JpaRepository<LocalAccountJpaEntity, Long> {
+
+    @Query("SELECT l FROM LocalAccountJpaEntity l WHERE l.accountId = :accountId")
+    Optional<LocalAccount> findByAccountId(@Param("accountId") String accountId);
 }
