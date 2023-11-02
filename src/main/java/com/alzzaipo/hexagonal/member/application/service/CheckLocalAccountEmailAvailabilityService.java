3c1da1e9e717f71401330eb5fa392ba62533319c
csct3434 <csct3434@gmail.com>
2023-11-03 05:19:04 +0900

    feat: 로컬 계정 이메일 중복 조회 어댑터 구현

---
+++ src/main/java/com/alzzaipo/hexagonal/member/application/service/CheckLocalAccountEmailAvailabilityService.java
@@ -0,0 +1,21 @@
+package com.alzzaipo.hexagonal.member.application.service;
+
+import com.alzzaipo.hexagonal.common.Email;
+import com.alzzaipo.hexagonal.member.application.port.in.CheckLocalAccountEmailAvailabilityQuery;
+import com.alzzaipo.hexagonal.member.application.port.out.FindLocalAccountByEmailPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CheckLocalAccountEmailAvailabilityService implements CheckLocalAccountEmailAvailabilityQuery {
+
+    private final FindLocalAccountByEmailPort findLocalAccountByEmailPort;
+
+    @Override
+    public boolean checkLocalAccountEmailAvailability(Email email) {
+        return findLocalAccountByEmailPort
+                .findLocalAccountByEmailPort(email)
+                .isEmpty();
+    }
+}
