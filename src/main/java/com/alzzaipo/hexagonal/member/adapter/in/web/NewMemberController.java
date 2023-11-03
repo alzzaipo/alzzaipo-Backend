8e95c6f0e9d91a18f170d4893015096790d64588
csct3434 <csct3434@gmail.com>
2023-11-04 03:41:39 +0900

    feat: 로컬 계정 아이디 중복 조회 웹 API 구현

--- src/main/java/com/alzzaipo/hexagonal/member/adapter/in/web/NewMemberController.java
+++ src/main/java/com/alzzaipo/hexagonal/member/adapter/in/web/NewMemberController.java
@@ -1,13 +1,13 @@
 package com.alzzaipo.hexagonal.member.adapter.in.web;
 
+import com.alzzaipo.hexagonal.member.application.port.in.CheckLocalAccountIdAvailabilityQuery;
 import com.alzzaipo.hexagonal.member.application.port.in.RegisterLocalAccountCommand;
 import com.alzzaipo.hexagonal.member.application.port.in.RegisterLocalAccountUseCase;
+import com.alzzaipo.hexagonal.member.domain.LocalAccount.LocalAccountId;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/new/member")
@@ -15,6 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class NewMemberController {
 
     private final RegisterLocalAccountUseCase registerLocalAccountUseCase;
+    private final CheckLocalAccountIdAvailabilityQuery checkLocalAccountIdAvailabilityQuery;
+
+    @GetMapping("/verify-account-id")
+    public ResponseEntity<String> verifyAccountId(@RequestParam("accountId") String accountId) {
+        LocalAccountId localAccountId = new LocalAccountId(accountId);
+
+        if (!checkLocalAccountIdAvailabilityQuery.checkLocalAccountIdAvailability(localAccountId)) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("이미 등록된 아이디 입니다.");
+        }
+
+        return ResponseEntity.ok().body("사용 가능한 아이디 입니다.");
+    }
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterLocalAccountWebRequest dto) {
