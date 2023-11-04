7f9eeecc0db7b15ab15861e2abc8789948be029a
csct3434 <csct3434@gmail.com>
2023-11-05 04:15:10 +0900

    refactor: 클래스명 변경(LocalLoginResult -> LoginResult)

--- src/main/java/com/alzzaipo/hexagonal/member/application/port/in/LocalLoginUseCase.java
+++ src/main/java/com/alzzaipo/hexagonal/member/application/port/in/LocalLoginUseCase.java
@@ -1,9 +1,9 @@
 package com.alzzaipo.hexagonal.member.application.port.in;
 
 import com.alzzaipo.hexagonal.member.application.port.in.dto.LocalLoginCommand;
-import com.alzzaipo.hexagonal.member.application.port.in.dto.LocalLoginResult;
+import com.alzzaipo.hexagonal.member.application.port.in.dto.LoginResult;
 
 public interface LocalLoginUseCase {
 
-    LocalLoginResult handleLocalLogin(LocalLoginCommand command);
+    LoginResult handleLocalLogin(LocalLoginCommand command);
 }
--- src/main/java/com/alzzaipo/hexagonal/member/application/port/in/dto/LoginResult.java
+++ src/main/java/com/alzzaipo/hexagonal/member/application/port/in/dto/LoginResult.java
@@ -3,17 +3,17 @@ package com.alzzaipo.hexagonal.member.application.port.in.dto;
 import lombok.Getter;
 
 @Getter
-public class LocalLoginResult {
+public class LoginResult {
 
     private final boolean success;
     private final String token;
 
-    public LocalLoginResult(boolean success, String token) {
+    public LoginResult(boolean success, String token) {
         this.success = success;
         this.token = token;
     }
 
-    public static LocalLoginResult getFailedResult() {
-        return new LocalLoginResult(false, "");
+    public static LoginResult getFailedResult() {
+        return new LoginResult(false, "");
     }
 }
