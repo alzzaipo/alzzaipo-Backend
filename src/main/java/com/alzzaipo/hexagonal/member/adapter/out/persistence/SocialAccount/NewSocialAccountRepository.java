30e6910085ddf59e645f01f2180a4323a53904d8
csct3434 <csct3434@gmail.com>
2023-11-05 02:11:57 +0900

    feat: 소셜 계정 조회 어댑터 구현

--- src/main/java/com/alzzaipo/hexagonal/member/adapter/out/persistence/SocialAccount/NewSocialAccountRepository.java
+++ src/main/java/com/alzzaipo/hexagonal/member/adapter/out/persistence/SocialAccount/NewSocialAccountRepository.java
@@ -1,6 +1,16 @@
 package com.alzzaipo.hexagonal.member.adapter.out.persistence.SocialAccount;
 
+import com.alzzaipo.hexagonal.common.LoginType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface NewSocialAccountRepository extends JpaRepository<SocialAccountJpaEntity, Long> {
+
+    @Query("SELECT s FROM SocialAccountJpaEntity s WHERE s.loginType = :loginType AND s.email = :email")
+    Optional<SocialAccountJpaEntity> findByLoginTypeAndEmail(
+            @Param("loginType") LoginType loginType,
+            @Param("email") String email);
 }
--- src/main/java/com/alzzaipo/hexagonal/member/adapter/out/persistence/SocialAccount/SocialAccountPersistenceAdapter.java
+++ src/main/java/com/alzzaipo/hexagonal/member/adapter/out/persistence/SocialAccount/SocialAccountPersistenceAdapter.java
@@ -1,8 +1,13 @@
 package com.alzzaipo.hexagonal.member.adapter.out.persistence.SocialAccount;
 
+import com.alzzaipo.hexagonal.common.Email;
+import com.alzzaipo.hexagonal.common.LoginType;
+import com.alzzaipo.hexagonal.common.Uid;
 import com.alzzaipo.hexagonal.member.adapter.out.persistence.Member.MemberJpaEntity;
 import com.alzzaipo.hexagonal.member.adapter.out.persistence.Member.NewMemberRepository;
+import com.alzzaipo.hexagonal.member.application.port.out.FindSocialAccountPort;
 import com.alzzaipo.hexagonal.member.application.port.out.RegisterSocialAccountPort;
+import com.alzzaipo.hexagonal.member.application.port.out.dto.FindSocialAccountCommand;
 import com.alzzaipo.hexagonal.member.domain.SocialAccount.SocialAccount;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -13,7 +18,9 @@ import java.util.Optional;
 @Component
 @Transactional
 @RequiredArgsConstructor
-public class SocialAccountPersistenceAdapter implements RegisterSocialAccountPort {
+public class SocialAccountPersistenceAdapter implements
+        RegisterSocialAccountPort,
+        FindSocialAccountPort {
 
     private final NewMemberRepository memberRepository;
     private final NewSocialAccountRepository socialAccountRepository;
@@ -31,10 +38,27 @@ public class SocialAccountPersistenceAdapter implements RegisterSocialAccountPor
         return optionalMemberJpaEntity.isPresent();
     }
 
+    @Override
+    public Optional<SocialAccount> findSocialAccount(FindSocialAccountCommand command) {
+        LoginType loginType = command.getLoginType();
+        String email = command.getEmail().get();
+
+        return socialAccountRepository.findByLoginTypeAndEmail(loginType, email)
+                .map(this::toDomainEntity);
+    }
+
     private SocialAccountJpaEntity toJpaEntity(MemberJpaEntity memberJpaEntity, SocialAccount socialAccount) {
         return new SocialAccountJpaEntity(
                 socialAccount.getEmail().get(),
                 socialAccount.getLoginType(),
                 memberJpaEntity);
+    }
+
+    private SocialAccount toDomainEntity(SocialAccountJpaEntity socialAccountJpaEntity) {
+        Uid memberUID = new Uid(socialAccountJpaEntity.getMemberJpaEntity().getUid());
+        Email email = new Email(socialAccountJpaEntity.getEmail());
+        LoginType loginType = socialAccountJpaEntity.getLoginType();
+
+        return new SocialAccount(memberUID, email, loginType);
     }
 }
