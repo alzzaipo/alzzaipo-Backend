fa0c774326ee46a612410299fcbd01fbde50c562
csct3434 <csct3434@gmail.com>
2023-11-05 20:17:13 +0900

    feat: 카카오 계정 연동 웹 API 구현

--- src/main/java/com/alzzaipo/hexagonal/member/adapter/in/web/OAuthController.java
+++ src/main/java/com/alzzaipo/hexagonal/member/adapter/in/web/OAuthController.java
@@ -1,10 +1,14 @@
 package com.alzzaipo.hexagonal.member.adapter.in.web;
 
+import com.alzzaipo.hexagonal.common.MemberPrincipal;
+import com.alzzaipo.hexagonal.common.Uid;
 import com.alzzaipo.hexagonal.member.application.port.in.KakaoLoginUseCase;
+import com.alzzaipo.hexagonal.member.application.port.in.LinkKakaoAccountUseCase;
 import com.alzzaipo.hexagonal.member.application.port.in.dto.AuthorizationCode;
 import com.alzzaipo.hexagonal.member.application.port.in.dto.LoginResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OAuthController {
 
     private final KakaoLoginUseCase kakaoLoginUseCase;
+    private final LinkKakaoAccountUseCase linkKakaoAccountUseCase;
 
     @GetMapping("/kakao/login")
     public ResponseEntity<String> kakaoLogin(@RequestParam("code") String authCode) {
@@ -31,5 +36,19 @@ public class OAuthController {
                     .body("로그인 성공");
         }
         return ResponseEntity.badRequest().body("로그인 실패");
+    }
+
+    @GetMapping("/kakao/link")
+    public ResponseEntity<String> kakaoLink(@AuthenticationPrincipal MemberPrincipal principal,
+                                            @RequestParam("code") String authCode) {
+        AuthorizationCode authorizationCode = new AuthorizationCode(authCode);
+        Uid memberUID = principal.getMemberUID();
+
+        boolean success = linkKakaoAccountUseCase.linkKakaoAccountUseCase(memberUID, authorizationCode);
+
+        if (success) {
+            return ResponseEntity.ok().body("연동 완료");
+        }
+        return ResponseEntity.badRequest().body("연동 실패");
     }
 }
