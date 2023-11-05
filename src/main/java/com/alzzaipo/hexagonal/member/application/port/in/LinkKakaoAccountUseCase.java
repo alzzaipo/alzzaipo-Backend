66ac8e71bbc7341db7154cc072fca573d98f9861
csct3434 <csct3434@gmail.com>
2023-11-05 20:15:08 +0900

    feat: 카카오 계정 연동 유스케이스 작성

---
+++ src/main/java/com/alzzaipo/hexagonal/member/application/port/in/LinkKakaoAccountUseCase.java
@@ -0,0 +1,9 @@
+package com.alzzaipo.hexagonal.member.application.port.in;
+
+import com.alzzaipo.hexagonal.common.Uid;
+import com.alzzaipo.hexagonal.member.application.port.in.dto.AuthorizationCode;
+
+public interface LinkKakaoAccountUseCase {
+
+    boolean linkKakaoAccountUseCase(Uid memberUID, AuthorizationCode authorizationCode);
+}
