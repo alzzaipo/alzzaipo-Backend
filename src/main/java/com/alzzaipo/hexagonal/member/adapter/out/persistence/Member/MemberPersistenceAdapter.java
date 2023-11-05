6e8c3e40bcbf81b2b9e452532979812dc57fb1b5
csct3434 <csct3434@gmail.com>
2023-11-05 22:07:20 +0900

    feat: 회원(Member) 조회 어댑터 구현

--- src/main/java/com/alzzaipo/hexagonal/member/adapter/out/persistence/Member/MemberPersistenceAdapter.java
+++ src/main/java/com/alzzaipo/hexagonal/member/adapter/out/persistence/Member/MemberPersistenceAdapter.java
@@ -1,13 +1,21 @@
 package com.alzzaipo.hexagonal.member.adapter.out.persistence.Member;
 
+import com.alzzaipo.hexagonal.common.Uid;
+import com.alzzaipo.hexagonal.member.application.port.out.FindMemberPort;
 import com.alzzaipo.hexagonal.member.application.port.out.RegisterMemberPort;
 import com.alzzaipo.hexagonal.member.domain.Member.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Component
+@Transactional
 @RequiredArgsConstructor
-public class MemberPersistenceAdapter implements RegisterMemberPort {
+public class MemberPersistenceAdapter implements
+        RegisterMemberPort,
+        FindMemberPort {
 
     private final NewMemberRepository memberRepository;
 
@@ -15,5 +23,18 @@ public class MemberPersistenceAdapter implements RegisterMemberPort {
     public void registerMember(Member member) {
         MemberJpaEntity memberJpaEntity = new MemberJpaEntity(member.getUid().get(), member.getNickname());
         memberRepository.save(memberJpaEntity);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Member> findMember(Uid uid) {
+        return memberRepository.findByUid(uid.get())
+                .map(this::toDomainEntity);
+    }
+
+    private Member toDomainEntity(MemberJpaEntity memberJpaEntity) {
+        return new Member(
+                new Uid(memberJpaEntity.getUid()),
+                memberJpaEntity.getNickname());
     }
 }
