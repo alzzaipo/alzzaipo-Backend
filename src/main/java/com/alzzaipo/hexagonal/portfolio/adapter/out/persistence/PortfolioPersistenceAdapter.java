2b4529cb987f67a355edc67b3458c1f53b0bd719
csct3434 <csct3434@gmail.com>
2023-11-06 21:23:56 +0900

    feat: 포트폴리오 등록 어댑터 구현

--- src/main/java/com/alzzaipo/hexagonal/portfolio/adapter/out/persistence/PortfolioPersistenceAdapter.java
+++ src/main/java/com/alzzaipo/hexagonal/portfolio/adapter/out/persistence/PortfolioPersistenceAdapter.java
@@ -1,5 +1,11 @@
 package com.alzzaipo.hexagonal.portfolio.adapter.out.persistence;
 
+import com.alzzaipo.hexagonal.ipo.adapter.out.persistence.IpoJpaEntity;
+import com.alzzaipo.hexagonal.ipo.adapter.out.persistence.NewIpoRepository;
+import com.alzzaipo.hexagonal.member.adapter.out.persistence.member.MemberJpaEntity;
+import com.alzzaipo.hexagonal.member.adapter.out.persistence.member.NewMemberRepository;
+import com.alzzaipo.hexagonal.portfolio.application.out.RegisterPortfolioPort;
+import com.alzzaipo.hexagonal.portfolio.domain.Portfolio;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,7 +13,39 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 @Transactional
 @RequiredArgsConstructor
-public class PortfolioPersistenceAdapter {
+public class PortfolioPersistenceAdapter implements RegisterPortfolioPort {
 
     private final NewPortfolioRepository portfolioRepository;
+    private final NewIpoRepository ipoRepository;
+    private final NewMemberRepository memberRepository;
+
+    @Override
+    public void registerPortfolio(Portfolio portfolio) {
+        MemberJpaEntity memberJpaEntity = memberRepository.findByUid(portfolio.getMemberUID().get())
+                .orElseThrow(() -> new RuntimeException("회원 조회 실패"));
+
+        IpoJpaEntity ipoJpaEntity = ipoRepository.findByStockCode(portfolio.getStockCode())
+                .orElseThrow(() -> new RuntimeException("공모주 조회 실패"));
+
+        PortfolioJpaEntity portfolioJpaEntity = toJpaEntity(
+                memberJpaEntity,
+                ipoJpaEntity,
+                portfolio);
+
+        portfolioRepository.save(portfolioJpaEntity);
+    }
+
+    private PortfolioJpaEntity toJpaEntity(MemberJpaEntity memberJpaEntity,
+                                           IpoJpaEntity ipoJpaEntity,
+                                           Portfolio portfolio) {
+        return new PortfolioJpaEntity(
+                portfolio.getPortfolioUID().get(),
+                portfolio.getSharesCnt(),
+                portfolio.getProfit(),
+                portfolio.getProfitRate(),
+                portfolio.getAgents(),
+                portfolio.getMemo(),
+                memberJpaEntity,
+                ipoJpaEntity);
+    }
 }
