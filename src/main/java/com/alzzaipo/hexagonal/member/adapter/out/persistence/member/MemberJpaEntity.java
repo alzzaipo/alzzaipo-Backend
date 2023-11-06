452c7d42815461b8f6767aa60de12a16cfae6610
csct3434 <csct3434@gmail.com>
2023-11-06 21:13:03 +0900

    feat: 회원과 포트폴리오 영속성 엔티티 간 연관 관계 설정

--- src/main/java/com/alzzaipo/hexagonal/member/adapter/out/persistence/member/MemberJpaEntity.java
+++ src/main/java/com/alzzaipo/hexagonal/member/adapter/out/persistence/member/MemberJpaEntity.java
@@ -2,6 +2,7 @@ package com.alzzaipo.hexagonal.member.adapter.out.persistence.member;
 
 import com.alzzaipo.hexagonal.member.adapter.out.persistence.account.local.LocalAccountJpaEntity;
 import com.alzzaipo.hexagonal.member.adapter.out.persistence.account.social.SocialAccountJpaEntity;
+import com.alzzaipo.hexagonal.portfolio.adapter.out.persistence.PortfolioJpaEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,6 +25,9 @@ public class MemberJpaEntity {
 
     @OneToMany(mappedBy = "memberJpaEntity", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<SocialAccountJpaEntity> socialAccountJpaEntities;
+
+    @OneToMany(mappedBy = "memberJpaEntity", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<PortfolioJpaEntity> portfolioJpaEntities;
 
     public MemberJpaEntity(Long uid, String nickname) {
         this.uid = uid;
