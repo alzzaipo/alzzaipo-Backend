9c3861b946467f4072ea6a0103c2ef17bf0e9eb0
csct3434 <csct3434@gmail.com>
2023-11-08 04:51:04 +0900

    chore: 구버전 클래스 및 테스트 삭제

--- src/main/java/com/alzzaipo/domain/notification/criteria/NotificationCriteriaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.alzzaipo.domain.notification.criteria;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationCriteriaRepository extends JpaRepository<NotificationCriteria, Long> {
-}
--- src/main/java/com/alzzaipo/enums/MemberType.java
+++
@@ -1,5 +0,0 @@
-package com.alzzaipo.enums;
-
-public enum MemberType {
-    LOCAL, SOCIAL
-}
