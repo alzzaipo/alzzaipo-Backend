3603e85f3ee59684800c34bc337180d8111ef8ef
csct3434 <csct3434@gmail.com>
2023-11-07 09:03:18 +0900

    feat: 이메일 알림 조회 어댑터 구현

--- src/main/java/com/alzzaipo/hexagonal/notification/adapter/out/persistence/email/EmailNotificationPersistenceAdapter.java
+++ src/main/java/com/alzzaipo/hexagonal/notification/adapter/out/persistence/email/EmailNotificationPersistenceAdapter.java
@@ -1,11 +1,29 @@
 package com.alzzaipo.hexagonal.notification.adapter.out.persistence.email;
 
+import com.alzzaipo.hexagonal.common.Email;
+import com.alzzaipo.hexagonal.common.Uid;
+import com.alzzaipo.hexagonal.notification.application.port.out.email.FindEmailNotificationPort;
+import com.alzzaipo.hexagonal.notification.domain.email.EmailNotification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
-public class EmailNotificationPersistenceAdapter {
+public class EmailNotificationPersistenceAdapter implements FindEmailNotificationPort {
 
     private final NewEmailNotificationRepository emailNotificationRepository;
+
+    @Override
+    public Optional<EmailNotification> findEmailNotification(Uid memberUID) {
+        return emailNotificationRepository.findByMemberUID(memberUID.get())
+                .map(this::toDomainEntity);
+    }
+
+    private EmailNotification toDomainEntity(EmailNotificationJpaEntity jpaEntity) {
+        return new EmailNotification(
+                new Uid(jpaEntity.getMemberJpaEntity().getUid()),
+                new Email(jpaEntity.getEmail()));
+    }
 }
