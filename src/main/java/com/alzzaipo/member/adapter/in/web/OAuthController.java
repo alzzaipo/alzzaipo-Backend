2157779d18a59b432a0dfe14d9887c5360a25da0
csct3434 <csct3434@gmail.com>
2023-11-12 13:09:49 +0900

    feat: 카카오 소셜 계정 연동 해지 웹 API 구현

--- src/main/java/com/alzzaipo/member/adapter/in/web/OAuthController.java
+++ src/main/java/com/alzzaipo/member/adapter/in/web/OAuthController.java
@@ -1,17 +1,17 @@
 package com.alzzaipo.member.adapter.in.web;
 
+import com.alzzaipo.common.LoginType;
 import com.alzzaipo.common.MemberPrincipal;
+import com.alzzaipo.member.application.port.in.account.social.UnlinkSocialAccountUseCase;
 import com.alzzaipo.member.application.port.in.dto.AuthorizationCode;
 import com.alzzaipo.member.application.port.in.dto.LoginResult;
+import com.alzzaipo.member.application.port.in.dto.UnlinkSocialAccountCommand;
 import com.alzzaipo.member.application.port.in.oauth.KakaoLoginUseCase;
 import com.alzzaipo.member.application.port.in.oauth.LinkKakaoAccountUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/oauth")
@@ -20,6 +20,7 @@ public class OAuthController {
 
     private final KakaoLoginUseCase kakaoLoginUseCase;
     private final LinkKakaoAccountUseCase linkKakaoAccountUseCase;
+    private final UnlinkSocialAccountUseCase unlinkSocialAccountUseCase;
 
     @GetMapping("/kakao/login")
     public ResponseEntity<String> kakaoLogin(@RequestParam("code") String authCode) {
@@ -45,5 +46,16 @@ public class OAuthController {
                 new AuthorizationCode(authCode));
 
         return ResponseEntity.ok().body("연동 완료");
+    }
+
+    @DeleteMapping("/kakao/unlink")
+    public ResponseEntity<String> kakaoUnlink(@AuthenticationPrincipal MemberPrincipal principal) {
+        UnlinkSocialAccountCommand command = new UnlinkSocialAccountCommand(
+                principal.getMemberUID(),
+                LoginType.KAKAO);
+
+        unlinkSocialAccountUseCase.unlinkSocialAccountUseCase(command);
+
+        return ResponseEntity.ok().body("해지 완료");
     }
 }
