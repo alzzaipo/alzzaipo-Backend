69c04a56a45d169ec09d610fbabae6290d7c4420
csct3434 <csct3434@gmail.com>
2023-11-21 23:26:19 +0900

    fix: 리스트 미할당으로 인한 NPE 문제 해결

--- src/main/java/com/alzzaipo/member/adapter/out/persistence/member/MemberJpaEntity.java
+++ src/main/java/com/alzzaipo/member/adapter/out/persistence/member/MemberJpaEntity.java
@@ -5,41 +5,46 @@ import com.alzzaipo.member.adapter.out.persistence.account.local.LocalAccountJpa
 import com.alzzaipo.member.adapter.out.persistence.account.social.SocialAccountJpaEntity;
 import com.alzzaipo.notification.adapter.out.persistence.criterion.NotificationCriterionJpaEntity;
 import com.alzzaipo.portfolio.adapter.out.persistence.PortfolioJpaEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor
 public class MemberJpaEntity extends BaseTimeEntity {
 
-    @Id
-    private Long uid;
+	@Id
+	private Long uid;
 
-    @Column(nullable = false)
-    private String nickname;
+	@Column(nullable = false)
+	private String nickname;
 
-    @OneToOne(mappedBy = "memberJpaEntity", cascade = CascadeType.REMOVE, orphanRemoval = true)
-    private LocalAccountJpaEntity localAccountJpaEntity;
+	@OneToOne(mappedBy = "memberJpaEntity", cascade = CascadeType.REMOVE, orphanRemoval = true)
+	private LocalAccountJpaEntity localAccountJpaEntity;
 
-    @OneToMany(mappedBy = "memberJpaEntity", cascade = CascadeType.REMOVE, orphanRemoval = true)
-    private List<SocialAccountJpaEntity> socialAccountJpaEntities;
+	@OneToMany(mappedBy = "memberJpaEntity", cascade = CascadeType.REMOVE, orphanRemoval = true)
+	private List<SocialAccountJpaEntity> socialAccountJpaEntities = new ArrayList<>();
 
-    @OneToMany(mappedBy = "memberJpaEntity", cascade = CascadeType.REMOVE, orphanRemoval = true)
-    private List<PortfolioJpaEntity> portfolioJpaEntities;
+	@OneToMany(mappedBy = "memberJpaEntity", cascade = CascadeType.REMOVE, orphanRemoval = true)
+	private List<PortfolioJpaEntity> portfolioJpaEntities = new ArrayList<>();
 
-    @OneToMany(mappedBy = "memberJpaEntity", cascade = CascadeType.REMOVE, orphanRemoval = true)
-    private List<NotificationCriterionJpaEntity> notificationCriteriaJpaEntities;
+	@OneToMany(mappedBy = "memberJpaEntity", cascade = CascadeType.REMOVE, orphanRemoval = true)
+	private List<NotificationCriterionJpaEntity> notificationCriteriaJpaEntities = new ArrayList<>();
 
-    public MemberJpaEntity(Long uid, String nickname) {
-        this.uid = uid;
-        this.nickname = nickname;
-    }
+	public MemberJpaEntity(Long uid, String nickname) {
+		this.uid = uid;
+		this.nickname = nickname;
+	}
 
-    public void changeNickname(String nickname) {
-        this.nickname = nickname;
-    }
+	public void changeNickname(String nickname) {
+		this.nickname = nickname;
+	}
 }
