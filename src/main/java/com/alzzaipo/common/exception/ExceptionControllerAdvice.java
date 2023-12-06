8087b0cb25a418bf0d47105dbd4faa269fed29c8
csct3434 <csct3434@gmail.com>
2023-12-06 20:38:21 +0900

    feat: MethodArgumentNotValidException 예외 처리 추가

--- src/main/java/com/alzzaipo/common/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/alzzaipo/common/exception/ExceptionControllerAdvice.java
@@ -1,10 +1,13 @@
 package com.alzzaipo.common.exception;
 
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,29 +20,38 @@ public class ExceptionControllerAdvice {
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
-        logger.error("MissingServletRequestParameterException: ", e.getMessage());
+        logger.error("MissingServletRequestParameterException: {}", e.getMessage());
         ErrorResponse errorResponse = new ErrorResponse("요청 파라미터 누락 오류", e.getMessage());
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
-        logger.error("MethodArgumentTypeMismatchException: ", e.getMessage());
+        logger.error("MethodArgumentTypeMismatchException: {}", e.getMessage());
         ErrorResponse errorResponse = new ErrorResponse("요청 파라미터 타입 오류", e.getMessage());
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        logger.error("HttpMessageNotReadableException: ", e.getMessage());
+        logger.error("HttpMessageNotReadableException: {}", e.getMessage());
         ErrorResponse errorResponse = new ErrorResponse("메시지 파싱 오류", e.getMessage());
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
-        logger.error("HttpRequestMethodNotSupportedException: ", e.getMessage());
+        logger.error("HttpRequestMethodNotSupportedException: {}", e.getMessage());
         ErrorResponse errorResponse = new ErrorResponse("HTTP 메서드 오류", e.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+        MethodArgumentNotValidException e) {
+        logger.error("MethodArgumentNotValidException : {}", e.getMessage());
+        String errorMessage = createValidationErrorMessage(e.getBindingResult());
+        ErrorResponse errorResponse = new ErrorResponse("입력 유효성 오류", errorMessage);
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
@@ -57,4 +69,10 @@ public class ExceptionControllerAdvice {
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
+    private String createValidationErrorMessage(BindingResult bindingResult) {
+        return bindingResult.getFieldErrors()
+            .stream()
+            .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+            .collect(Collectors.joining(", "));
+    }
 }
