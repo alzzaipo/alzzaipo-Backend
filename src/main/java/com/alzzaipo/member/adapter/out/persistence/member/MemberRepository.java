6c75a26ef9a94a5507b28899fb2d850075b4cfc0
csct3434 <csct3434@gmail.com>
2023-12-08 22:38:17 +0900

    chore: 불필요한 개행 정리

--- src/main/java/com/alzzaipo/member/adapter/out/persistence/member/MemberRepository.java
+++ src/main/java/com/alzzaipo/member/adapter/out/persistence/member/MemberRepository.java
@@ -3,7 +3,6 @@ package com.alzzaipo.member.adapter.out.persistence.member;
 import com.alzzaipo.member.adapter.out.persistence.member.custom.MemberRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<MemberJpaEntity, Long>,
-	MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<MemberJpaEntity, Long>, MemberRepositoryCustom {
 
 }
