d753695cc2041344b71870b62ed0b184b21873e1
csct3434 <csct3434@gmail.com>
2023-12-14 02:42:02 +0900

    refactor: 인가 방식 변경으로 인한 공모주 스크래핑 컨트롤러 코드 정리
    - 인가 방식 변경 : 코드 검증 -> Role 검증
    - 불필요한 인가 관련 코드 제거

--- src/main/java/com/alzzaipo/ipo/adapter/in/web/IpoScrapingController.java
+++ src/main/java/com/alzzaipo/ipo/adapter/in/web/IpoScrapingController.java
@@ -5,8 +5,6 @@ import com.alzzaipo.ipo.application.port.in.ScrapeAndRegisterIposUseCase;
 import com.alzzaipo.ipo.application.port.out.dto.ScrapeIposCommand;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,28 +16,10 @@ public class IpoScrapingController {
 
 	private final ScrapeAndRegisterIposUseCase scrapeAndRegisterIposUseCase;
 
-	@Value("${crawler.verificationCode}")
-	private String authorizationCode;
-
 	@PostMapping("/scraper")
-	public ResponseEntity<String> scrapeAndRegisterIpos(
-		@Valid @RequestBody ScrapeAndRegisterIposWebRequest scrapeAndRegisterIposWebRequest) {
-
-		if (!checkAuthorizationCode(scrapeAndRegisterIposWebRequest.getAuthorizationCode())) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증 실패");
-		}
-
-		ScrapeIposCommand scrapeIposCommand = new ScrapeIposCommand(
-			scrapeAndRegisterIposWebRequest.getPageFrom(),
-			scrapeAndRegisterIposWebRequest.getPageTo());
-
-		int totalRegistrationCount =
-			scrapeAndRegisterIposUseCase.scrapeAndRegisterIposUseCase(scrapeIposCommand);
-
+	public ResponseEntity<String> scrapeAndRegisterIpos(@Valid @RequestBody ScrapeAndRegisterIposWebRequest dto) {
+		ScrapeIposCommand command = new ScrapeIposCommand(dto.getPageFrom(), dto.getPageTo());
+		int totalRegistrationCount = scrapeAndRegisterIposUseCase.scrapeAndRegisterIposUseCase(command);
 		return ResponseEntity.ok("[성공] " + totalRegistrationCount + "건 업데이트 완료");
-	}
-
-	private boolean checkAuthorizationCode(String userInputAuthorizationCode) {
-		return userInputAuthorizationCode.equals(authorizationCode);
 	}
 }
--- src/main/java/com/alzzaipo/ipo/adapter/in/web/dto/ScrapeAndRegisterIposWebRequest.java
+++ src/main/java/com/alzzaipo/ipo/adapter/in/web/dto/ScrapeAndRegisterIposWebRequest.java
@@ -1,7 +1,6 @@
 package com.alzzaipo.ipo.adapter.in.web.dto;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,12 +10,9 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ScrapeAndRegisterIposWebRequest {
 
-    @NotBlank
-    private String authorizationCode;
+	@Min(value = 1)
+	private int pageFrom;
 
-    @Min(value = 1)
-    private int pageFrom;
-
-    @Min(value = 1)
-    private int pageTo;
+	@Min(value = 1)
+	private int pageTo;
 }
